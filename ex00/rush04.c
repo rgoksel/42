8aa4290c8ce6f95d88d363fcc382e036197c3a7c
rgoksel <118483956+rgoksel@users.noreply.github.com>
2023-03-26 17:17:09 +0300

    Add files via upload

---
+++ ex00/rush04.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdemiray <rdemiray@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 11:12:57 by rdemiray          #+#    #+#             */
+/*   Updated: 2023/03/26 14:19:09 by rdemiray         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	error(void)
+{
+	write(1, "ERROR - ", 8);
+	write(1, "pozitif tam sayı giriniz", 25);
+	ft_putchar('\n');
+}
+
+void	parca(int basla, int orta, int son, int sutun_sayisi)
+{
+	ft_putchar(basla);
+	sutun_sayisi --;
+	while (sutun_sayisi > 1)
+	{
+		ft_putchar(orta);
+		sutun_sayisi--;
+	}
+	if (sutun_sayisi == 1)
+		ft_putchar(son);
+}
+
+void	rush(int satirx, int sutuny)
+{
+	int	i;
+
+	if (satirx > 0 && sutuny > 0)
+	{
+		i = 1;
+		while (i <= satirx)
+		{
+			if (i == 1)
+				parca('A', 'B', 'C', sutuny);
+			else if (i == satirx)
+				parca('C', 'B', 'A', sutuny);
+			else
+				parca('B', ' ', 'B', sutuny);
+			i++;
+			ft_putchar('\n');
+		}
+	}
+	else if (satirx <= 0 || sutuny <= 0)
+		error();
+}
